6e68a1afbff6257638fca3f3f93354b06065b439
Sameed0 <sameedu786@gmail.com>
2020-06-25 22:35:12 +0500

    remove build file from git ignore

---
+++ bootcamp-react-app/build/precache-manifest.40b0d4dc7bb3cdc60d4fae19a813a768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3887342ae78311ed4af31ff271eecba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50c3fafe239510e57fbf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "898087cff7defe93501d",
+    "url": "/static/js/2.aa65c6fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa65c6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c3fafe239510e57fbf",
+    "url": "/static/js/main.fa9280d4.chunk.js"
+  },
+  {
+    "revision": "7dd51c7abf6bda0b6e43",
+    "url": "/static/js/runtime-main.2d7e3f53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
